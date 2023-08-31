1893e9cd5642b86e1516e5bf076815920701736c
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2023-08-31 13:49:31 -0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    > -->
+    <h1>Home</h1>
+    {{ data }}
+  </q-page>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+import { api } from 'src/boot/axios'
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+});
+
+
+</script>
