5296c15e6b53131635566e5d7a9e5384d7a00a27
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2023-11-11 12:25:34 -0300

    Added About page

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,6 +14,13 @@ const routes = [
       { path: '', component: () => import('pages/LoginPage.vue') }
     ]
   },
+  {
+    path: '/about',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/AboutPage.vue') }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
